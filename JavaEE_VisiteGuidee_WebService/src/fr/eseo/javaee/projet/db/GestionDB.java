8cdeb7be1b1a422f040caf6c2bdda9e57e39a270
guiguibubu <guibu44@hotmail.fr>
2018-03-16 17:45:34 +0100

    changement date string en objet date

--- JavaEE_VisiteGuidee_WebService/src/fr/eseo/javaee/projet/db/GestionDB.java
+++ JavaEE_VisiteGuidee_WebService/src/fr/eseo/javaee/projet/db/GestionDB.java
@@ -2,10 +2,14 @@ package fr.eseo.javaee.projet.db;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class GestionDB {
 
 	static BaseDeDonnees bdd;
+
+	private GestionDB() {};
 
 	private static void initConnection() throws SQLException {
 		bdd = BaseDeDonnees.getInstance();
@@ -29,7 +33,7 @@ public class GestionDB {
 	public static boolean existeClient(String prenom, String nom) throws SQLException {
 		boolean existeClient = false;
 		initConnection();
-		String sql = "SELECT * FROM client";
+		String sql = "SELECT * FROM client WHERE prenom=\""+prenom+"\" AND nom=\""+nom+"\"";
 		ResultSet rs = bdd.executeSQL(sql, true);
 		existeClient = rs.next();
 		bdd.closeResulSet();
@@ -56,10 +60,10 @@ public class GestionDB {
 		}
 	}
 
-	public static void updateClient(String prenom, String nom, String dateNaissance, String adresse, int codePostal, int num_tel, String mail) throws SQLException {
+	public static void updateClient(String prenom, String nom, LocalDate dateNaissance, String adresse, int codePostal, int numTel, String mail) throws SQLException {
 		if(existeClient(prenom, nom)) {
 			initConnection();
-			String sql = "UPDATE client SET dateNaissance='"+dateNaissance+"', adresse='"+adresse+"', codePostal='"+codePostal+"', numTelephone='"+num_tel+"', mail='"+mail+"' WHERE prenom='"+prenom+"' AND nom='"+nom+"'";
+			String sql = "UPDATE client SET dateNaissance='"+bdd.convertDateForDB(dateNaissance)+"', adresse='"+adresse+"', codePostal='"+codePostal+"', numTelephone='"+numTel+"', mail='"+mail+"' WHERE prenom='"+prenom+"' AND nom='"+nom+"'";
 			bdd.executeSQL(sql, false);
 			closeConnection();
 		}
@@ -67,10 +71,21 @@ public class GestionDB {
 
 	//METHODES VISITES
 
-	public static boolean existeVisite(String typeVisite, String ville, String date) throws SQLException {
+	public static boolean searchVisite(String typeVisite, String ville, LocalDateTime date) throws SQLException {
+		// TODO : changer sortie en objet arraylist de visite
 		boolean existeVisite = false;
+		boolean whereClause = false;
 		initConnection();
 		String sql = "SELECT * FROM visite";
+
+		if(!"".equals(typeVisite)) {
+			if(!whereClause) {
+				sql += " WHERE";
+				whereClause = true;
+			}
+			sql += " typeVisite = \""+typeVisite+"\"";
+		}
+
 		ResultSet rs = bdd.executeSQL(sql, true);
 		existeVisite = rs.next();
 		bdd.closeResulSet();
@@ -79,17 +94,21 @@ public class GestionDB {
 		return existeVisite;
 	}
 
-	public static void ajoutVisite(String type, String ville, String date, int prix) throws SQLException {
+	public static boolean existeVisite(String typeVisite, String ville, LocalDateTime date) throws SQLException {
+		return searchVisite(typeVisite, ville, date);
+	}
+
+	public static void ajoutVisite(String type, String ville, LocalDateTime date, int prix) throws SQLException {
 		if(!existeVisite(type, ville, date)) {
 			initConnection();
-			String sql = "INSERT INTO visite (typeVisite, ville, date, prix) VALUES ('"+type+"','"+ville+"','"+date+"','"+prix+"')";
+			String sql = "INSERT INTO visite (typeVisite, ville, date, prix) VALUES ('"+type+"','"+ville+"','"+bdd.convertDateTimeForDB(date)+"','"+prix+"')";
 			bdd.executeSQL(sql, false);
 			closeConnection();
 		}
 	}
 
 
-	public static void supprimerVisite(String type, String ville, String date) throws SQLException {
+	public static void supprimerVisite(String type, String ville, LocalDateTime date) throws SQLException {
 		if(!existeVisite(type, ville, date)) {
 			initConnection();
 			String sql = "DELETE FROM visite WHERE typeVisite='"+type+"' AND ville='"+ville+"' AND date='"+date+"')";
