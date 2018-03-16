d06f18874c50a9a76543dae9cef809b333eee4ab
lubuntu <lubuntu@lubuntu-VirtualBox>
2018-03-16 17:28:05 +0100

    requete SQL pour visite et reservation

--- JavaEE_VisiteGuidee_WebService/src/fr/eseo/javaee/projet/db/GestionDB.java
+++ JavaEE_VisiteGuidee_WebService/src/fr/eseo/javaee/projet/db/GestionDB.java
@@ -56,18 +56,21 @@ public class GestionDB {
 		}
 	}
 
-	public static void updateClient(String prenom, String nom, String dateNaissance, String adresse, int codePostal, int num_tel, String mail) throws SQLException {
+	public static void updateClient(String prenom, String nom, String dateNaissance, String adresse, int codePostal,
+			int num_tel, String mail) throws SQLException {
 		if(existeClient(prenom, nom)) {
 			initConnection();
-			String sql = "UPDATE client SET dateNaissance='"+dateNaissance+"', adresse='"+adresse+"', codePostal='"+codePostal+"', numTelephone='"+num_tel+"', mail='"+mail+"' WHERE prenom='"+prenom+"' AND nom='"+nom+"'";
+			String sql = "UPDATE client SET dateNaissance='"+dateNaissance+"', adresse='"+adresse+"', "
+					+ "codePostal='"+codePostal+"', numTelephone='"+num_tel+"', mail='"+mail+"' WHERE "
+					+ "prenom='"+prenom+"' AND nom='"+nom+"'";
 			bdd.executeSQL(sql, false);
 			closeConnection();
 		}
 	}
-	
-	
-	
-	
+
+
+
+	// TODO : tester (sera fait samedi/dimanche)
 	public static boolean existeVisite(String typeVisite, String ville, String date) throws SQLException {
 		boolean existeVisite = false;
 		initConnection();
@@ -83,7 +86,8 @@ public class GestionDB {
 	public static void ajoutVisite(String type, String ville, String date, int prix) throws SQLException {
 		if(!existeVisite(type, ville, date)) {
 			initConnection();
-			String sql = "INSERT INTO visite (typeVisite, ville, date, prix) VALUES ('"+type+"','"+ville+"','"+date+"','"+prix+"')";
+			String sql = "INSERT INTO visite (typeVisite, ville, date, prix) VALUES "
+					+ "('"+type+"','"+ville+"','"+date+"','"+prix+"')";
 			bdd.executeSQL(sql, false);
 			closeConnection();
 		}
@@ -91,13 +95,63 @@ public class GestionDB {
 
 
 	public static void supprimerVisite(String type, String ville, String date) throws SQLException {
-		if(!existeVisite(type, ville, date)) {
+		if(existeVisite(type, ville, date)) {
 			initConnection();
-			String sql = "DELETE FROM visite WHERE typeVisite='"+type+"' AND ville='"+ville+"' AND date='"+date+"')";
+			String sql = "DELETE FROM visite WHERE typeVisite='"+type+"' AND ville='"+ville+"' AND date='"+date+"'";
 			bdd.executeSQL(sql, false);
 			closeConnection();
 		}
 	}
-	
-	
+
+	public static boolean existeReservation(int idVisite, int idClient) throws SQLException {
+		boolean existeReservation = false;
+		initConnection();
+		String sql = "SELECT * FROM reservation";
+		ResultSet rs = bdd.executeSQL(sql, true);
+		existeReservation = rs.next();
+		bdd.closeResulSet();
+		bdd.closeStatement();
+		closeConnection();
+		return existeReservation;
+	}
+
+	public static void ajouterReservation(int idVisite, int idClient) throws SQLException {
+		if(!existeReservation(idVisite, idClient)) {
+			initConnection();
+			String sql = "INSERT INTO visite (idVisite, idClient) VALUES ('"+idVisite+"','"+idClient+"')";
+			bdd.executeSQL(sql, false);
+			closeConnection();
+		}
+	}
+
+	public static void supprimerReservation(int idVisite, int idClient) throws SQLException {
+		if(existeReservation(idVisite, idClient)) {
+			initConnection();
+			String sql = "DELETE FROM reservation WHERE idVisite='"+idVisite+"' AND idClient='"+idClient+"'";
+			bdd.executeSQL(sql, false);
+			closeConnection();
+		}
+	}
+
+
+	public static int[] trouverReservationPlaceLibre(int places) throws SQLException {
+		int[] id_reservation_dispo = null;
+		initConnection();
+		String sql = "SELECT idVisite FROM reservation WHERE nombrePlaces >= '"+places+"'";
+		//ResultSet rs = bdd.executeSQL(sql);
+		//id_reservation_dispo = rs.next();
+		return id_reservation_dispo;
+	}
+
+	public static boolean reservationVilleDate(String ville, String date) throws SQLException {
+		boolean existeReservationVille = false;
+		initConnection();
+		String sql = "SELECT reservation.idReservation FROM reservation, visite WHERE reservation.idVisite = visite.idVisite AND visite.ville='"+ville+"' AND visite.date='"+date+"'";
+		ResultSet rs = bdd.executeSQL(sql, true);
+		existeReservationVille = rs.next();
+		bdd.closeResulSet();
+		bdd.closeStatement();
+		closeConnection();
+		return existeReservationVille;
+	}
 }
