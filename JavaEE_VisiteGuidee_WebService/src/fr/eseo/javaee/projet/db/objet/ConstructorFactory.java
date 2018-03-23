7698ced9813ed8d2dd072c33706442653dfe17bb
guiguibubu <guibu44@hotmail.fr>
2018-03-23 15:08:43 +0100

    exemple avec reservation

--- JavaEE_VisiteGuidee_WebService/src/fr/eseo/javaee/projet/db/objet/ConstructorFactory.java
+++ JavaEE_VisiteGuidee_WebService/src/fr/eseo/javaee/projet/db/objet/ConstructorFactory.java
@@ -2,4 +2,28 @@ package fr.eseo.javaee.projet.db.objet;
 
 public class ConstructorFactory {
 
+	//RESERVATION
+
+	public static Reservation createReservation(int codeReservation, Visite visite, Client client, int nombrePersonnes, boolean paiementEffectue) {
+		Reservation reservation = new Reservation();
+		reservation.setCodeReservation(codeReservation);
+		reservation.setVisite(visite);
+		reservation.setClient(client);
+		reservation.setNombrePersonnes(nombrePersonnes);
+		reservation.setPaiementEffectue(paiementEffectue);
+		return reservation;
+	}
+
+	public static Reservation createReservation(Visite visite, Client client, int nombrePersonnes, boolean paiementEffectue) {
+		return createReservation(0, visite, client, nombrePersonnes, paiementEffectue);
+	}
+
+	public static Reservation createReservation(int idReservation) {
+		return createReservation(idReservation, null,null,0,false);
+	}
+
+	public static Reservation createReservation() {
+		return createReservation(0);
+	}
+
 }
